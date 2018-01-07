2b564d387c5d8e0a22c4229548852f90bd6c3e0d
phil <phil@cyberstack.uk>
2018-01-07 12:00:22 +0000

    added print to spread calc

--- graph/spread_calculator.py
+++ graph/spread_calculator.py
@@ -8,8 +8,7 @@ class SpreadCalculator(object):
 		print ('Starting with %s. Rate = %d. After moving through amount = %d' % (path[0]['arc'],path[0]['rate'],current_amount))
 		while (i < len(path) - 1):
 			current_amount = self.move_through(current_amount, path[i]['arc'], path[i]['rate'])
-			#print 
-			#print('After moving through ' + str(path[i]['arc']) + ', amount = ' + current_amount)
+			print ('After moving through %s. Amount = %d' % (path[i]['arc'], current_amount))
 			i += 1
 		return current_amount
 
