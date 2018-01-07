e7acb5eed113f82dcf06e2af4d0a40b236f54eb0
phil <phil@cyberstack.uk>
2018-01-07 12:58:12 +0000

    Adds threaded path finding

--- elefund.py
+++ elefund.py
@@ -3,13 +3,16 @@
 from pricer import Pricer
 from graph import Market
 from fees import Calculator
+from graph import PathFinder
 
 market = Market()
 fee_store = {}
 
 #pricer = Pricer(market).start()
+PathFinder(market).start()
 
 Calculator(fee_store).start()
+
 
 print('fees')
 print(fee_store)
--- graph/path_finder.py
+++ graph/path_finder.py
@@ -4,30 +4,35 @@ from .arc import Arc
 class PathFinder(object):
 
 	spread_calculator = SpreadCalculator()
-	hard_coded = []
-	strategies = []
-
-	#subscriber = SubscriberManager()
-
-	def __init__(self, graph):
-		self.graph = graph
-		self.hard_coded = [
+	hard_coded = [
 			Arc('ETH', 'BTC', 'Kraken'),
 			Arc('BTC', 'XRP', 'Kraken'),
 			Arc('XRP', 'XRP', 'Kraken'),
 			Arc('XRP', 'BTC', 'Qryptos'),
 			Arc('BTC', 'ETH', 'Qryptos'),
 			Arc('ETH', 'ETH', 'Qryptos')
-		]
-	strategies = [
-		hard_coded
 	]
+	updated_arcs = []
+	strategies = []
+
+
+	def __init__(self, graph):
+		self.graph = graph
+		self.strategies = [
+			hard_coded
+		]
+
+	def start():
+		while True:
+			arc = self.updated_arcs.get()
+			for each in self.hard_coded:
+				if each == arc:
+					spread = self.calc_path_spread(self.hard_coded)
+					print spread
+
 
 	def arc_updated(self, arc):
-		for each in self.hard_coded:
-			if each == arc:
-				spread = self.calc_path_spread(self.hard_coded)
-				print (spread)
+		updated_arcs.append(arc)
 
 
 	def calc_path_spread(self, path):
