a68b5d292590beddabd6e130c08251b915295d6d
Alejandro U. Alvarez <aluralma@gmail.com>
2018-02-04 15:46:50 +0000

    move common stuff to the state

--- elefund.py
+++ elefund.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3
 
 from pricer import Pricer
-from graph import Market
 from fees import Calculator
 from graph import PathFinder
-from multiprocessing import Queue
+
+from state import market, updated_arcs, fee_store
 
 
-updated_arcs = Queue(maxsize=1000) #shared queue between strats and graph
-market = Market(updated_arcs)
 path_finder = PathFinder(market, updated_arcs).start_pricing()
-
-
-fee_store = {}
-
 pricer = Pricer(market).start()
-
-
 Calculator(fee_store).start()
-
 
 print('fees')
 print(fee_store)
--- state/__init__.py
+++ state/__init__.py
@@ -1,4 +1,6 @@
 from graph import Market
+from multiprocessing import Queue
 
-#market = Market()
+updated_arcs = Queue(maxsize=1000) #shared queue between strats and graph
+market = Market(updated_arcs)
 fee_store = {}
